e9bb663bb392b19243cf7f9810a659d040383dd0
Tomohiro Hieda <16nc056@ms.dendai.ac.jp>
2016-10-07 02:12:14 +0900

    コミットハッシュ6898f59の状態まで復元

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 socket.io を使ったチャットアプリケーションです。
 
+## 必要なモジュール
+
+ログの格納に__NeDB__モジュールを利用します。  
+https://github.com/louischatriot/nedb  
+
+ログ記録/読込時にタイムスタンプを利用するため__Date-Utils__モジュールを利用します。  
+https://github.com/JerrySievert/date-utils/
+
+コマンドライン引数の処理に__argv__モジュールを利用します。  
+https://www.npmjs.com/package/argv
+
 ## 使い方
 
 サーバーの起動:
@@ -13,3 +24,19 @@ $ nodejs server.js
 ```
 $ nodejs client.js
 ```
+
+ログイン時に出力されるログの最大数を設定する場合:
+
+```
+$ nodejs client.ls -l value
+```
+デフォルトでは15件です。  
+
+## ログの出力形式
+
+```
+[ MM/DD HH24:MI ]user: message_...
+[ MM/DD HH24:MI ]user: message_before_last
+[ MM/DD HH24:MI ]user: message_last
+```
+
--- client.js
+++ client.js
@@ -1,6 +1,18 @@
 var io = require('socket.io-client');
 var socket = io('http://localhost:3000');
+var argv = require( 'argv' );
 var readline = require('readline');
+
+//コマンドラインオプションを定義
+argv.option([
+    {
+    name: 'log',
+    short: 'l',
+    type: 'int',
+    description: 'ログイン時に表示されるログ数を指定します',
+    example: '"node client.js log --log=balue" or "node client.js -l value"'
+    },
+]);
 
 var rl = readline.createInterface({
     input: process.stdin,
@@ -16,7 +28,7 @@ function askUserName() {
     rl.prompt();
     
     rl.once('line', function(line) {
-        socket.emit('user login', line.trim());
+        socket.emit('user login', line.trim(), max_output_log);
     });
 }
 
@@ -60,4 +72,13 @@ socket.on('say', function(data) {
     log('%s: %s', data.name, data.message);
 });
 
+socket.on('show_log', function(data) {
+    log('[ %s ] %s: %s', data.date, data.name, data.message);
+});
+
+var args = argv.run();
+var max_output_log = 15;
+if(args.options.log != undefined){
+    max_output_log = args.options.log;
+}
 askUserName();
